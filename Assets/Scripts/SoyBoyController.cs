24ce354bd2c189c705861dce1585fb75b2e41ade
Stelios Klapsis <Steliosklapsis@outlook.com>
2022-12-07 18:38:20 -0500

    Chapter 13 - Added Anim Triggers To SoyBoy Controller

--- Assets/Scripts/SoyBoyController.cs
+++ Assets/Scripts/SoyBoyController.cs
@@ -119,6 +119,9 @@ public class SoyBoyController : MonoBehaviour
         // 1
         input.x = Input.GetAxis("Horizontal");
         input.y = Input.GetAxis("Jump");
+
+        animator.SetFloat("Speed", Mathf.Abs(input.x));
+
         // 2
         if (input.x > 0f)
         {
@@ -132,10 +135,12 @@ public class SoyBoyController : MonoBehaviour
         if (input.y >= 1f)
         {
             jumpDuration += Time.deltaTime;
+            animator.SetBool("IsJumping", true);
         }
         else
         {
             isJumping = false;
+            animator.SetBool("IsJumping", false);
             jumpDuration = 0f;
         }
 
@@ -145,6 +150,7 @@ public class SoyBoyController : MonoBehaviour
             {
                 isJumping = true;
             }
+            animator.SetBool("IsOnWall", false);
         }
 
         if (jumpDuration > jumpDurationThreshold) input.y = 0f;
@@ -185,16 +191,23 @@ public class SoyBoyController : MonoBehaviour
         }
 
         // 3
-        rb.AddForce(new Vector2(((input.x * speed) - rb.velocity.x)
-                                * acceleration, 0));
+        rb.AddForce(new Vector2(((input.x * speed) - rb.velocity.x) * acceleration, 0));
         // 4
         rb.velocity = new Vector2(xVelocity, yVelocity);
-
-        if (IsWallToLeftOrRight() && !PlayerIsOnGround()
-                                  && input.y == 1)
+        if (IsWallToLeftOrRight() && !PlayerIsOnGround() && input.y == 1)
         {
-            rb.velocity = new Vector2(-GetWallDirection()
-                                      * speed * 0.75f, rb.velocity.y);
+            rb.velocity = new Vector2(-GetWallDirection() * speed * 0.75f, rb.velocity.y);
+            animator.SetBool("IsOnWall", false);
+            animator.SetBool("IsJumping", true);
+        }
+        else if (!IsWallToLeftOrRight())
+        {
+            animator.SetBool("IsOnWall", false);
+            animator.SetBool("IsJumping", true);
+        }
+        if (IsWallToLeftOrRight() && !PlayerIsOnGround())
+        {
+            animator.SetBool("IsOnWall", true);
         }
 
         if (isJumping && jumpDuration < jumpDurationThreshold)
